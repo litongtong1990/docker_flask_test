7647e3e9712280c3c0cc4468163fe7ac5ca21003
yichengli <liyichengtt@gmail.com>
2017-07-21 14:14:38 +0800

    xxx

--- myproject.py
+++ myproject.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'This is to test the port'
+    return 'This is an end-2-end test'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
