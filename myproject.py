ff98d5ba758ec939e5359743c5ecff4f6f99dfe2
yichengli <liyichengtt@gmail.com>
2017-07-21 10:50:40 +0800

    test auto deploy

--- myproject.py
+++ myproject.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'hook test,This is a test page in Docker image test2'
+    return 'Test auto deploy,This is a test page in Docker image test2'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
