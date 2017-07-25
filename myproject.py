20c485596be9d241d6f2bdc9df93ea138a0926ca
yichengli <liyichengtt@gmail.com>
2017-07-25 11:10:26 +0800

    demo test

--- myproject.py
+++ myproject.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'This is an 0724 test'
+    return 'This is an 1111 test'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
